02ce7e340e934d69a9dfc8eecb93db275220c1c5
Markus <markus.mork@iths.se>
2022-11-28 12:35:43 +0100

    Style margins

--- src/TimeFilter.tsx
+++ src/TimeFilter.tsx
@@ -7,10 +7,13 @@ const TimeFilter= (): JSX.Element => {
       return options
     }
       return (
-        <select style={{padding: "10px", margin: "auto", width: "50%"}}>
+        // <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+
+        <select style={{padding: "10px", margin: "10px 10px 10px 20px", width: "50%"}}>
            {renderOptions()}
             <option></option>
           </select>
+        // </div>
       );
 
 
